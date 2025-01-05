f995d4961877d50ab66d012966f6035c269afad4
Klaudiusz Pastor <pastor.klo12@gmail.com>
2025-01-05 16:35:58 +0100

    lab5 update android basic app

---
+++ lab5/ButtonReactionApp/app/src/main/java/com/example/buttonreactionapp/MainActivity.java
@@ -0,0 +1,42 @@
+package com.example.buttonreactionapp;
+import com.example.buttonreactionapp.MainActivity2;
+
+import android.os.Bundle;
+import android.widget.Toast;
+
+import androidx.activity.EdgeToEdge;
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.graphics.Insets;
+import androidx.core.view.ViewCompat;
+import androidx.core.view.WindowInsetsCompat;
+import android.content.Intent;
+import android.widget.Button;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        EdgeToEdge.enable(this);
+        setContentView(R.layout.activity_main);
+
+        setupWindowInsets();
+
+        Button button = findViewById(R.id.button);
+        button.setOnClickListener(v -> onButtonClicked());
+    }
+
+    private void setupWindowInsets() {
+        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main), (v, insets) -> {
+            Insets systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars());
+            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
+            return insets;
+        });
+    }
+
+    private void onButtonClicked() {
+        Toast.makeText(this, "Button clicked!", Toast.LENGTH_LONG).show();
+        Intent intent = new Intent(this, MainActivity2.class);
+        startActivity(intent);
+    }
+}
---
+++ lab5/ButtonReactionApp/app/src/main/java/com/example/buttonreactionapp/MainActivity2.java
@@ -0,0 +1,38 @@
+package com.example.buttonreactionapp;
+
+import android.os.Bundle;
+import androidx.activity.EdgeToEdge;
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.graphics.Insets;
+import androidx.core.view.ViewCompat;
+import androidx.core.view.WindowInsetsCompat;
+
+import android.content.Intent;
+import android.widget.Button;
+
+public class MainActivity2 extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        EdgeToEdge.enable(this);
+        setContentView(R.layout.activity_main2);
+        setupWindowInsets();
+
+        Button buttonBack = findViewById(R.id.button2);
+        buttonBack.setOnClickListener(v -> onBackButtonClicked());
+
+    }
+    private void setupWindowInsets() {
+        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main), (v, insets) -> {
+            Insets systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars());
+            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
+            return insets;
+        });
+    }
+
+    private void onBackButtonClicked() {
+        Intent intent = new Intent(this, MainActivity.class);
+        startActivity(intent);
+    }
+}
